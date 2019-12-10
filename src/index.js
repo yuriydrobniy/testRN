6ce5c7257ea7ed0d1c5448be5abeb0435d3ebf3f
Yu <yu@KYI-Drobniy-Y.local>
2019-12-10 17:26:16 +0200

    monor test

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export default class Intro extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>
           This is a React Native snapshot test.
         </Text>
